9e678124b66d279d3d1b399d23126fd5065894d2
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-06-18 09:53:30 -0600

    add filter by name to get method

--- api/api_views.py
+++ api/api_views.py
@@ -9,6 +9,9 @@ class SalariesAPIView(GenericAPIView):
     serializer_class = SalariesSerializer
 
     def get(self, request, *args, **kwargs):
+        name = self.request.GET.get('name', None)
+        if name:
+            self.queryset = self.queryset.filter(name__icontains=name)
         queryset = self.paginate_queryset(queryset=self.queryset)
         serializer = SalariesSerializer(instance=queryset, many=True)
         return self.get_paginated_response(serializer.data)
