b15d5955d237c61a3b2d5391ff0d445408866dc0
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-06-18 22:13:58 -0600

    Add post method with CreateModelMixin

--- api/api_views.py
+++ api/api_views.py
@@ -1,10 +1,12 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.response import Response
 
 from api.models import Salaries
 from api.serializers import SalariesSerializer
 
 
-class SalariesAPIView(GenericAPIView):
+class SalariesAPIView(CreateModelMixin, GenericAPIView):
     queryset = Salaries.objects.all()
     serializer_class = SalariesSerializer
 
@@ -15,3 +17,6 @@ class SalariesAPIView(GenericAPIView):
         queryset = self.paginate_queryset(queryset=self.queryset)
         serializer = SalariesSerializer(instance=queryset, many=True)
         return self.get_paginated_response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
