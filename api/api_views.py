c9db31d0ae3579c50b6b4af55e9472ee4ce92d4d
Stephen Castaneda <scastaneda@pivottechschool.com>
2020-09-29 18:37:14 -0500

    first question done

--- api/api_views.py
+++ api/api_views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import GenericAPIView
-
 from api.models import Salaries
 from api.serializers import SalariesSerializer
 
@@ -10,5 +9,8 @@ class SalariesAPIView(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         queryset = self.paginate_queryset(queryset=self.queryset)
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = self.paginate_queryset(queryset=self.queryset.filter(name__icontains=name))
         serializer = SalariesSerializer(instance=queryset, many=True)
         return self.get_paginated_response(serializer.data)
